e411537e9b5aec924e3a2058c1e0294a0fc86ce1
PREM SAGAR MALHOTRA <sagarprem2006@gmail.com>
2026-03-15 01:47:28 +0530

    Delete MAITRI_AI directory

--- MAITRI_AI/src/voice-processor/exports.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Voice Processor Module Exports
- * 
- * Public API for the voice processor module
- */
-
-export { TranscriptionService } from './transcription-service';
-export { handler, TranscriptionEvent, LambdaResponse } from './index';
-export type { TranscriptionServiceConfig } from './transcription-service';
